45b3fa1b930f6dbfe51c24c15ba44810f74dc4b2
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-12-13 11:25:25 +0900

    リクエスト送信箇所のコメントアウトを解除

--- main.py
+++ main.py
@@ -21,29 +21,29 @@ def submit_article(path):
     with open(f"{path}/item.md") as f:
         body = f.read()
         print(body)
-    # headers = {"Authorization": f"Bearer {TOKEN}"}
+    headers = {"Authorization": f"Bearer {TOKEN}"}
     item = json.loads(conf)
     item["body"] = body
     print(item)
 
-    # if item["id"] == "":
-    #     res = requests.post(BASE_URL, headers=headers, json=item)
-    #     with open(f"{path}/config.json", "w") as f:
-    #         item["id"] = res.json()["id"]
-    #         item["body"] = ""
-    #         f.write(json.dumps(item))
-    #     return res
+    if item["id"] == "":
+        res = requests.post(BASE_URL, headers=headers, json=item)
+        with open(f"{path}/config.json", "w") as f:
+            item["id"] = res.json()["id"]
+            item["body"] = ""
+            f.write(json.dumps(item))
+        return res
 
-    # else:
-    #     now = datetime.datetime.now()
-    #     item["title"] += now.strftime("【%Y/%m/%d %H時更新】")
-    #     item_id = item["id"]
-    #     res = requests.patch(BASE_URL + f"/{item_id}", headers=headers, json=item)
-    #     return res
+    else:
+        now = datetime.datetime.now()
+        item["title"] += now.strftime("【%Y/%m/%d %H時更新】")
+        item_id = item["id"]
+        res = requests.patch(BASE_URL + f"/{item_id}", headers=headers, json=item)
+        return res
 
 
 if __name__ == "__main__":
     # res = submit_article("article", conf, body).json()
     print("main")
     res = submit_article("article")
-    # print(res["title"], res["url"])
+    print(res["title"], res["url"])
